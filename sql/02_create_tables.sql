d2471c6bff8e5fb05936f97b69206a38d96ceff6
jjjjiaying888-dot <jjjjiaying888@gmail.com>
2026-01-03 20:51:23 +0800

    Add 系统设置 table

--- sql/02_create_tables.sql
+++ sql/02_create_tables.sql
@@ -11,12 +11,21 @@
 -- 6. 每日考勤
 -- 7. 缺勤通报
 
+--部门
 CREATE TABLE 部门
 (部门编号 int primary key,
  部门名称 nchar(30))
 select*from 部门
+--员工
 CREATE TABLE 员工
 (员工编号 int primary key,
  员工姓名 nchar(30),
  部门编号 int foreign key references 部门(部门编号))
 select*from 员工
+--系统设置
+CREATE TABLE 系统设置
+(设置编号 int,
+ 上班时间 time(0),
+ 下班时间 time(0),
+ 加班时间 time(0))
+select*from 系统设置
